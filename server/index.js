75b8dc24d7b1dd58e0dc749eb3cfef63317053eb
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2022-06-22 00:11:36 +0530

    Update & Minor Fixes

--- server/index.js
+++ server/index.js
@@ -1,30 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenev from "dotenv";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenev.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-
 app.use("/posts", postRoutes);
-
-const CONNECTION_URL =
-  "mongodb+srv://its-ikd:QxAIE7kAEXkGw49L@cluster0.aiume.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(`${error} did not connect`));
 
 // https://www.mongodb.com/cloud/atlas
-
-
-
