2f0ba98f480ee4bec657d2d84aee40b126830515
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2022-08-03 00:27:46 +0530

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Avatar,
-  Button,
-  Paper,
-  Grid,
-  Typography,
-  Container,
-} from "@material-ui/core";
+import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 
 import Input from "./Input";
@@ -17,8 +10,7 @@ const Auth = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignup] = useState(false);
 
-  const handleShowPassword = () =>
-    setShowPassword((prevShowPassword) => !prevShowPassword);
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
   const handleSubmit = () => {};
 
@@ -36,8 +28,7 @@ const Auth = () => {
   useEffect(() => {
     // global google
     google.accounts.id.initialize({
-      client_id:
-        "336085599881-bqa9blk7ar7k0oq1dficdheo26erftt1.apps.googleusercontent.com",
+      client_id: "336085599881-bqa9blk7ar7k0oq1dficdheo26erftt1.apps.googleusercontent.com",
       callback: googleSuccess,
     });
 
@@ -58,27 +49,11 @@ const Auth = () => {
           <Grid container spacing={2}>
             {isSignup && (
               <>
-                <Input
-                  name="firstName"
-                  label="First Name"
-                  handleChange={handleChange}
-                  autoFocus
-                  half
-                />
-                <Input
-                  name="lastName"
-                  label="Last Name"
-                  handleChange={handleChange}
-                  half
-                />
+                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                <Input name="lastName" label="Last Name" handleChange={handleChange} half />
               </>
             )}
-            <Input
-              name="email"
-              label="Email Address"
-              handleChange={handleChange}
-              type="email"
-            />
+            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
             <Input
               name="password"
               label="Password"
@@ -86,39 +61,17 @@ const Auth = () => {
               type={showPassword ? "text" : "password"}
               handleShowPassword={handleShowPassword}
             />
-            {isSignup && (
-              <Input
-                name="confirmPassword"
-                label="Repeat Password"
-                handleChange={handleChange}
-                type="password"
-              />
-            )}
+            {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
           </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
+          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
             {isSignup ? "Sign Up" : "Sign In"}
           </Button>
-          <Button
-            className={classes.googleButton}
-            id="googleSignIn"
-            fullWidth
-            variant="text"
-          >
+          <Button className={classes.googleButton} id="googleSignIn" fullWidth variant="text">
             Google Sign In
           </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
-              <Button onClick={switchMode}>
-                {isSignup
-                  ? "Already have an account? Sign in"
-                  : "Don't have an account? Sign Up"}
-              </Button>
+              <Button onClick={switchMode}>{isSignup ? "Already have an account? Sign in" : "Don't have an account? Sign Up"}</Button>
             </Grid>
           </Grid>
         </form>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -11,13 +11,7 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
-        <Typography
-          component={Link}
-          to="/"
-          className={classes.heading}
-          variant="h2"
-          align="center"
-        >
+        <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
@@ -25,32 +19,18 @@ const Navbar = () => {
       <div className={classes.toolbar}>
         {user ? (
           <div className={classes.profile}>
-            <Avatar
-              className={classes.purple}
-              alt={user.result.name}
-              src={user.result.imageUrl}
-            >
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
               {user.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant="h6">
               {user.result.name}
             </Typography>
-            <Button
-              variant="contained"
-              className={classes.logout}
-              color="secondary"
-              onClick={() => {}}
-            >
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={() => {}}>
               Logout
             </Button>
           </div>
         ) : (
-          <Button
-            component={Link}
-            to="/auth"  
-            variant="contained"
-            color="primary"
-          >
+          <Button component={Link} to="/auth" variant="contained" color="primary">
             Sign In
           </Button>
         )}
