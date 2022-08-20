d284997f9433ee3d26818c75d1212e563803b967
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2022-08-20 12:26:33 +0530

    Create User authentication in backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -31,8 +31,7 @@ export const updatePost = async (req, res) => {
   const { id } = req.params;
   const { title, message, creator, selectedFile, tags } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
   const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
@@ -43,8 +42,7 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
   const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
   await PostMessage.findByIdAndDelete(id);
 
@@ -53,15 +51,25 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
   const { id } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
+
+  // req.userId <--- coming from the auth middleware
+  if (!req.userId) return res.json({ message: "Unauthenticated" });
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
   const post = await PostMessage.findById(id);
-  const updatedPost = await PostMessage.findByIdAndUpdate(
-    id,
-    { likeCount: post.likeCount + 1 },
-    { new: true }
-  );
+
+  const index = post.likes.findIndex((id) => id === String(req.userId)); //if user id exists in the likes array
+
+  if (index === -1) {
+    // like the post
+    post.likes.push(req.userId);
+  } else {
+    //dislike the post
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
   res.json(updatedPost);
 };
