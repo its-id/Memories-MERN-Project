9413d44fca52893e434c12a54d9e3ce0522fb7e1
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2022-12-12 00:06:56 +0530

    Update API endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://ikd-memories.onrender.com/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
