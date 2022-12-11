5e5bdbaee8b3295e67fb1066c963e7bbd5eff2b9
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2022-12-11 22:56:55 +0530

    Add Procfile

--- server/index.js
+++ server/index.js
@@ -16,7 +16,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -25,9 +29,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() =>
-    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
-  )
+  .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 // https://www.mongodb.com/cloud/atlas
